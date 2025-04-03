50e9489c37646d348b863332595e77dbff605b84
Aarav-Code-tech <kartikajay.gupta@gmail.com>
2025-04-03 11:10:18 +0530

    game1.py

--- game.py
+++ game.py
@@ -2,25 +2,24 @@ import random
 a = ["rock","paper","scissor"]
 b = random.choice(a)
 c = input("Enter rock paper or scissor : ")
+d = c.lower()
 print("Computers Choice :",b)
-if b==c:
-  prin   t("tie")
+if b==d:
+  print("tie")
 elif b=="rock":
-  if c == "paper":
-    print("The winner is",c)
+  if d == "paper":
+    print("You win")
   else:
     print("The winner is",b)
 elif b=="scissor":
-  if c == "rock":
-    print("The winner is",c)
+  if d == "rock":
+    print("You win")
   else:
     print("The winner is",b)
 elif b=="paper":
-  if c == "scissor":
-    print("The winner is",c)
+  if d == "scissor":
+    print("You win")
   else:
     print("The winner is",b)
 else:
   print("Invalid")
-    
-  
